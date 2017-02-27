e64b8fe906c83cd2bd9f72990b8b2f2dfc7ea102
vinoththecreator <s.vinoth305@gmail.com>
2017-02-27 13:03:09 +0530

    Talha project

---
+++ Prjmet/src/Clsmeth.java
@@ -0,0 +1,17 @@
+public class Clsmeth
+	{
+		int a, c;
+
+		public String Clsmeth(int b)
+			{
+				return ("Account details  " + b);
+			}
+
+		public String meth1(int c)
+			{
+				a = 11111111;
+				this.c = c;
+				int d = c - a;
+				return ("the a/c number is \t" + d);
+			}
+	}
---
+++ Prjmet/src/Clsmeths.java
@@ -0,0 +1,11 @@
+
+public class Clsmeths {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Clsmeth ob = new Clsmeth();
+ob.Clsmeth(2222222);
+ob.meth1(4444);
+	}
+
+}
